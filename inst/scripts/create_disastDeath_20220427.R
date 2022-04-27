f29a8cd769521b93e6cfd3a7115d0741001d1541
Catalina <catalina@Catalinas-MacBook-Air.local>
2022-04-27 11:53:52 -0400

    diaster death

--- inst/scripts/create_disastDeath_20220427.R
+++ inst/scripts/create_disastDeath_20220427.R
@@ -1,9 +1,9 @@
-# Create Table of Disaster Locations
+# Create Table of Disaster Mortality
 # Catalina Cañizares and Gabriel Odom
-# 04/26/2022
+# 04/27/2022
 
 # Now that we have a unique key for all events (by state, year, and month), we
-#   will create a table of geographic locations by event
+#   will create a table of the mortality for each event
 
 library(tidyDisasters)
 library(tidyverse)
@@ -13,28 +13,20 @@ allEventsMap_df <- readRDS(
 )
 # 36,224,806 x 5
 
-allLocations_df <-
-  allEventsMap_df %>%
-  group_by(femaID) %>%
-  fill(emdatID, .direction = "updown") %>%
-  ungroup() %>%
-  replace_na( list(femaID = "", emdatID = "") ) %>%
-  mutate(femaID_trunc = str_remove(femaID, pattern = "_[^.]*$")) %>%
-  mutate(smashedID = paste0(femaID_trunc, "_", emdatID)) %>%
-  select(-femaID_trunc) %>% 
-	select(smashedID, state, county) %>% 
-	distinct()
-# 279,149 x 3
+data("clean_emdat")
+# 3101152 x 9
 
 
-###  Map All Locations to Unique Key  ###
+###  Map All death data to Unique Key  ###
 data("allKeys_df")
-disastLocations_df <- 
-	left_join(allLocations_df, allKeys_df, by = "smashedID") %>% 
-	select(eventKey, state, county) %>% 
+disastMortality_df <- 
+  clean_emdat %>% 
+  rename(emdatID = region_id) %>% 
+	left_join(allKeys_df, clean_emdat, by = "emdatID") %>% 
+	select(eventKey, nkill) %>% 
 	distinct()
 # 279,149 x 3
 
-usethis::use_data(disastLocations_df)
+usethis::use_data()
 
 
